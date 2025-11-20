808812400c3b1f8c46d70cd75b18dff801ce44ad
Lamystere <l@lamystere.com>
2025-11-20 16:09:54 -0600

    moving repo accounts

--- src/include/assets.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASSETS_h
-#define __ASSETS_h
-
-extern const unsigned char SPLASH_IMG[];
-extern const unsigned char PLUG_ICON[][24*3];
-extern const unsigned char DOOM[];
-extern const unsigned char SIM_CITY[];
-
-#endif
--- src/version.c
+++
@@ -1,7 +0,0 @@
-const char* EOM_VERSION =
-#ifdef PROJECT_VER
-    PROJECT_VER
-#else
-    "2.0.0"
-#endif
-    ;
